c8faa3da14b2152c351952175df7fe13f9bf36eb
nataszkan <natasza@fingerspitz.se>
2017-02-19 23:06:01 +0100

    tutorial until no 12

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// hanterar komponeterna
+import ReactDom from 'react-dom';
+// interagerar med DOMet
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Create a new component. This componenet should produce html
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+
+
+
+// Take this component´s generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
