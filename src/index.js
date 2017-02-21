dda7070b665a9a3730f167493115e06fa2fc5b2f
nataszkan <natasza@fingerspitz.se>
2017-02-21 14:20:59 +0100

    JS: APP have access to the videos on state

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDu2u5oQk5jnsMeeFkQnaPUqyUnpRIPETA';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props)  {
+    super(props);
+
+      this.state = {  videos: []  };
+
+      YTSearch({key: API_KEY, term: 'surfboards'},  (videos)  =>  {
+        this.setState({ videos });
+        //if the key and the variable have the same name: this.setState({ videos:videos });
+      });
+   }
+
   render () {
     return (
       <div>
