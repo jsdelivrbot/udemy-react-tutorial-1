7568ba31a47cab79c369f5d90c67d079c31b38c7
nataszkan <natasza@fingerspitz.se>
2017-02-22 11:01:45 +0100

    JS: video detail renders list of videos, checks if video is loaded, then loads video detail + list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDu2u5oQk5jnsMeeFkQnaPUqyUnpRIPETA';
 
 class App extends Component {
@@ -16,11 +17,13 @@ class App extends Component {
         //if the key and the variable have the same name: this.setState({ videos:videos });
       });
    }
+   //när appen först renderas skapar den en tom videolista, sen går den direkt vidare och gör en förfrågan efter videos.
 
   render () {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
